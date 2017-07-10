c5f0d752abbe9bfcc37fc507e6e7a6bdc79f340a
AlexSokolskih <xaam1@ya.ru>
2017-07-10 09:39:28 +0300

    1

--- app/controllers/controller_404.php
+++ app/controllers/controller_404.php
@@ -5,6 +5,9 @@ class Controller_404 extends Controller
 
     public function action_index()
     {
+        header('HTTP/1.1 404 Not Found');
+        header('Status: 404 Not Found');
+
         $this->view->generate('base_view.twig',
             [
                  'title' => 'Ошибка 404!',
